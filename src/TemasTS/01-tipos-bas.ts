2064064643f5037acdaf9f4d24f74236ed2e677f
jhernandez <80326@alumnos.utleon.edu.mx>
2026-09-04 22:47:13 -0600

    Tipos de datos  04-09-2026

---
+++ src/TemasTS/01-tipos-bas.ts
@@ -0,0 +1,20 @@
+console.log("Hola Mundo");
+
+// Tres tipos de declaraciones de variables
+
+var num1 = 23;
+let num2 = 65;
+const num3 = 22;
+
+let nombre: string;
+let num: number;
+let activo: boolean;
+let sum: number = num1 + num2 + num3;
+
+console.log("La suma es: " + sum);
+
+nombre = "Pedro";
+console.log(nombre);
+
+console.log(nombre);
+console.log("Prueba");
